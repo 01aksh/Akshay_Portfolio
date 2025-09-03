9e5d325cf0b620f6b1e18640e1e94c79e626c575
unknown <akshaylokapur@gmail.com>
2025-09-03 11:29:45 +0530

    npm install gh-pages --save-dev  added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
+  basePath: "/Akshay_Portfolio",
+  assetPrefix: "/Akshay_Portfolio/",
   eslint: {
     ignoreDuringBuilds: true,
   },
